918edf563af31307eaa667b9c9f574db2c33db43
codervala <princeramani42@gmail.com>
2025-12-23 11:31:40 +0530

    heeeey

---
+++ db.sql
@@ -0,0 +1,18 @@
+xkjvcklvlk
+d;;ljfinal public function fskjgsifbskjfk,b
+khsiufgsufgsf
+'ffkjffs
+
+sn mnfnxlkfs
+s
+sfbjhsvjhs (parameters)
+{
+    # code...vnkghdiufhdsifhgbsfns
+    fslknfsjfbvsfs
+    ffsbfjhvfjfs
+    FSfkjsbfjsbvfkjsf
+    ssldbsjfdvsjhbd 
+}
+cfsioufhdsigf
+\d;lkgnddg
+]d]enkdbd
---
+++ user login .php
@@ -0,0 +1,15 @@
+xjknaiudgauydgauydds
+fclose
+fscanfsf
+set_file_buffers
+filter_var
+vdx
+xxfxxdvd
+
+
+variant_divxv
+<datagrid>
+    zjbzjhfcbsjfbs<?phpsbsjhfbvsjfbvs<?php
+    d,mvcskgcbjcbnc
+    fgnskjfskjfbsjhdgvsdgv
+</datagrid>
